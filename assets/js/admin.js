effc61b849abb463ab8913e20c08c308159f8b01
LeThanhHuy1710 <huyl93304@gmail.com>
2025-07-05 09:07:28 +0700

    Update admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,1 +1,7 @@
-
+<script>
+  db.collection("gdv_list").get().then(snapshot => {
+    console.log("Kết nối Firebase thành công, có", snapshot.size, "GDV");
+  }).catch((err) => {
+    console.error("Lỗi kết nối Firebase:", err);
+  });
+</script>
