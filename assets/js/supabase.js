8dd872ba9f18302e41f5ed01202ca774c0bf466b
LeThanhHuy1710 <huyl93304@gmail.com>
2026-01-03 16:06:27 +0700

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,9 +1,7 @@
-// assets/js/supabase.js
-
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 // Supabase client
 export const supabase = createClient(
-  "https://xeidegtzbbiuglgmkbsm.supabase.co",                 // Project URL
-  "sb_publishable_XqJzHKum27HwEEzDhxKAqQ_qdoItx4K"          // Publishable key
+  "https://xeidegtzbbiuglgmkbsm.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhlaWRlZ3R6YmJpdWdsZ21rYnNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc0MjA5ODYsImV4cCI6MjA4Mjk5Njk4Nn0.x7KraIEBvyYXyFcsPiVE9k2wCNfHIUWW9TgrhK6BBN8"
 );
